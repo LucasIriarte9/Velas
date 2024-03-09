b4765bef98d065202cdbc40f2989d2d583e68bad
LucasIriarte9 <lucasiriartematias@gmail.com>
2024-03-09 12:17:07 -0300

    Version 2.1

--- js/main.js
+++ js/main.js
@@ -1,89 +1,111 @@
-//mi array de objetos
-const productos =
-    [
-        {
-            id: 1, nombre: "difusor", precio: 1600, img: "../images/difusor.JPG",
-        },
-        {
-            id: 2, nombre: "vela aromatica", precio: 1200, img: "../images/perfuminas.WEBP",
-        },
-        {
-            id: 3, nombre: "corazon de yeso", precio: 1200, img: "../images/vela.JPG",
-        },
-        {
-            id: 4, nombre: "velas de noche", precio: 800, img: "../images/velaabajo.WEBP",
-        },
-        {
-            id: 5, nombre: "bombones", precio: 1000, img: "../images/bombones.JPG",
-        },
-        {
-            id: 6, nombre: "vaso chico", precio: 800, img: "../images/vaso.JPG",
-        },
-    ];
+const tienda = document.querySelector(".tienda");
+const btnCart = document.querySelector(".container-cart-icon");
+const containerCartProduct = document.querySelector(".container-cart-products");
+const cartInfo = document.querySelector(".cart-product");
+const rowProduct = document.querySelector(".row-product");
+const valortotal = document.querySelector(".total-pagar");
+const countProduct = document.querySelector("#contador-productos");
 
-//Funcion constructora para crear objetos
-function Producto(nombre, precio, img) {
-    this.id = productos.length + 1;
-    this.nombre = nombre;
-    this.precio = parseFloat(precio);
-    if (img) {
-        this.img = img;
-    } else {
-        this.img = "https://placehold.co/400";
-    }
-}
-//Agrego un objeto nuevo a mi array de objetos con el push
-const nuevoProducto = new Producto("vaso grande", 1200, "../images/vasoGrande.png");
-productos.push(nuevoProducto);
-
-//funcion para filtrar por nombre
-function buscarPorNombre(array, filtro) {
-    if (filtro == "") {
-        return 0;
-    } else {
-        return array.find(el => {
-            return el.nombre.includes(filtro);
+let carrito = [];
+//mostrar el carrito
+btnCart.addEventListener("click", () => {
+    containerCartProduct.classList.toggle("hidden-cart");
+})
+//---------------------------------------------------------------
+tienda.addEventListener("click", e => {
+    if (e.target.classList.contains("btn-add-cart")) {
+        const producte = (e.target.parentElement);
+        const infoProduct =
+        {
+            quantity: 1,
+            title: producte.querySelector("h2").textContent,
+            price: producte.querySelector("p").textContent
         }
-        )
+        const exits = carrito.some(producte => producte.title === infoProduct.title);
+        if (exits) {
+            const productes = carrito.map(producte => {
+                if (producte.title === infoProduct.title) {
+                    producte.quantity++;
+                    return producte
+                } else {
+                    return producte;
+                }
+            })
+            carrito = [...productes];
+        } else {
+            carrito = [...carrito, infoProduct];
+        }
+
+        showHtml();
     };
+});
+
+rowProduct.addEventListener("click", (e) => {
+    if (e.target.classList.contains("icon-close")) {
+        const producte = e.target.parentElement;
+        const title = producte.querySelector("p").textContent;
+
+        carrito = carrito.filter(producte => producte.title !== title);
+        showHtml();
+    }
+})
+//Funcion para mostrar html
+const showHtml = () => {
+    //limpiar html 
+    rowProduct.innerHTML = "";
+    let total = 0;
+    let totalOfProduct = 0;
+    carrito.forEach(producte => {
+        const containerProduct = document.createElement("div");
+        containerProduct.classList.add("cart-product");
+        containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                          <span class="cantidad-producto-carrito">${producte.quantity}</span>
+                          <p class="titulo-producto-carrito">${producte.title}</p>
+                          <span class="precio-producto-carrito">${producte.price}</span>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                          stroke="currentColor" class="icon-close">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+        
+                        `
+        rowProduct.append(containerProduct);
+        total = total + parseInt(producte.quantity * producte.price.slice(1));
+        totalOfProduct = totalOfProduct + producte.quantity;
+
+    });
+
+    valortotal.innerHTML = `$${total}`;
+    countProduct.innerHTML = totalOfProduct;
 }
 
-//condicional para verificar que el usuario busco algo
-let buscar = prompt("Que producto desea buscar:");
-if (buscar == "") {
-    alert("No busco nada, estos son todos nuestros productos");
-    productos.forEach((el) => console.log(el));
-} else {
-    const encontrado = buscarPorNombre(productos, buscar);
-    console.log(encontrado);
-}
 
-//funcion para filtrar por precio
-function filtrarPrecio(array, filtro) {
-    return array.filter(el => {
-        return el.precio <= filtro;
-    })
-}
-porPrecio = parseFloat(prompt("Filtrar por precio"));
-const masBaratos = filtrarPrecio(productos, porPrecio);
-console.log(masBaratos);
+//-----------------------------------------------------------------------------
 
-//funcion para agregar al carrito
-const carrito = [];
-function agregarCarrito(array, productoAgregar) {
-    array.push(productoAgregar);
-    return array;
-}
-let deseado = prompt("Que producto desea agregar al carrito");
-let productoDeseado = buscarPorNombre(productos, deseado)
-agregarCarrito(carrito, productoDeseado);
 
-//funcion para sumar el carrito
-function comprarProducto(products, envio) {
-    return products.reduce((acc, el) => {
-        return acc = acc + el.precio
-    },envio);
-}
-const compra = comprarProducto(carrito, 1500);
-console.log(compra);
+// //Funcion constructora para crear objetos
+// function Producto(nombre, precio, img) {
+//     this.id = productos.length + 1;
+//     this.nombre = nombre;
+//     this.precio = parseFloat(precio);
+//     if (img) {
+//         this.img = img;
+//     } else {
+//         this.img = "https://placehold.co/400";
+//     }
+// }
+// //Agrego un objeto nuevo a mi array de objetos con el push
+// const nuevoProducto = new Producto("vaso grande", 1200, "../images/vasoGrande.png");
+// productos.push(nuevoProducto);
 
+
+// //funcion para filtrar por precio
+// function filtrarPrecio(array, filtro) {
+//     return array.filter(el => {
+//         return el.precio <= filtro;
+//     })
+// }
+// porPrecio = parseFloat(prompt("Filtrar por precio"));
+// const masBaratos = filtrarPrecio(productos, porPrecio);
+// console.log(masBaratos);
